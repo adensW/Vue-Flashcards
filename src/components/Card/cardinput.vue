4e8724aa299797128a8a0d814fe55713317ab42e
adensW <1012790248@qq.com>
2018-10-09 19:16:36 +0800

    solve conflict

--- src/components/Card/cardinput.vue
+++ src/components/Card/cardinput.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <div>
-            
+            <input v-model="front">
+        </div>
+        <div>
+            <input v-model="back">
         </div>
     </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
