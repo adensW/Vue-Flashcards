6e1a0446533b2bae6fdb2e30b7080dd3a1078e23
adensW <1012790248@qq.com>
2018-11-05 16:14:12 +0800

    [feat] test press move function

--- src/components/CardDetail/CardList.vue
+++ src/components/CardDetail/CardList.vue
@@ -30,170 +30,178 @@
     </div>
 </template>
 <script>
-import CardListItem from './CardListItem'
-import {} from '@/plugins/atween.js'
-import {VueAnime} from 'vue-anime'
-import {dbcontext} from '@/service/context/dbcontext-class.js'
+import CardListItem from "./CardListItem";
+import {} from "@/plugins/atween.js";
+import { VueAnime } from "vue-anime";
+import { dbcontext } from "@/service/context/dbcontext-class.js";
 
 export default {
-    name:"CardList",
-    components:{
-        CardListItem,
-        VueAnime
+  name: "CardList",
+  components: {
+    CardListItem,
+    VueAnime
+  },
+  data() {
+    return {
+      loading: true,
+      error: false,
+      offset: 0,
+      isSlider: false,
+      cards: [],
+      threshold: 90,
+      index: 0
+    };
+  },
+
+  created() {
+    // this.init();
+  },
+  mounted() {
+    this.init();
+  },
+  watch: {
+    $route: "init"
+  },
+  methods: {
+    rotate: function(evt) {
+      console.log(evt);
     },
-    data(){
-        return{
-            loading:true,
-            error:false,
-            offset:0,
-            isSlider:false,
-            currentCardId:0,
-            cards:[]
+    pressMove: function(evt) {
+      if (Math.abs(this.offset) < this.threshold) {
+        this.offset += evt.deltaX;
+        let elem = document.getElementsByClassName("anim__slider");
+        let moveoffset =
+          this.offset * (21 / this.threshold) +
+          (this.offset / Math.abs(this.offset)) * 21 * this.index;
+        let cssstyle = elem.item(0).style;
+        cssstyle.transform = "translateX(" + moveoffset + "rem)";
+        console.log(moveoffset);
+      } else {
+        this.isSlider = !this.isSlider;
+        if (this.offset > 0) {
+          this.index -= 1;
+        } else {
+          this.index += 1;
         }
+        this.offset = 0;
+      }
     },
-   
-    created(){
-        // this.init();
+    swipe: function(evt) {
+      switch (evt.direction) {
+        case "Left":
+          if (this.currentCardId >= this.cards.length - 1) {
+            this.currentCtiardId = this.cards.length;
+          } else {
+            this.isSlider = true;
+            let elem = document.getElementsByClassName("anim__slider");
+
+            let cssstyle = elem.item(0).style;
+            this.offset = this.offset + -21;
+            cssstyle.transform = "translateX(" + this.offset + "rem)";
+          }
+
+          break;
+        case "Right":
+          if (this.currentCardId <= 0) {
+            this.currentCardId = 0 * 1;
+          } else {
+            this.isSlider = false;
+            let elem = document.getElementsByClassName("anim__slider");
+            let cssstyle = elem.item(0).style;
+            this.offset = this.offset + 21;
+            cssstyle.transform = "translateX(" + this.offset + "rem)";
+          }
+
+          break;
+        case "Up":
+          break;
+        case "Down":
+          break;
+      }
     },
-    mounted(){
-       this.init();
+    initCardStore(id) {
+      console.log(id);
     },
-    watch:{
-        '$route': 'init'
+    getInput: function(card, event) {
+      console.log(card.id);
+      console.log(event);
     },
-    methods:{
-        swipe: function(evt) {
-            switch(evt.direction){
-                case 'Left':
-                 if(this.currentCardId>=this.cards.length-1){
-                        this.currentCtiardId = this.cards.length;
-                    }else{
-                       
-                        this.currentCardId = this.cards[this.currentCardId+1].id
-                        this.isSlider=true; 
-                        let elem = document.getElementsByClassName("anim__slider")
-                         
-                        let cssstyle = elem.item(0).style
-                        this.offset=this.offset+(-21)
-                        cssstyle.transform="translateX("+this.offset+"rem)"
-                        
-                    }
-               
-                break;
-                case 'Right':
-                 if(this.currentCardId<=0){
-                        this.currentCardId=0*1;
-                    }else{
-                        this.currentCardId = this.cards[this.currentCardId-1].id
-                        this.isSlider=false;
-                        let elem = document.getElementsByClassName("anim__slider")
-                        let cssstyle = elem.item(0).style
-                        this.offset= this.offset+(21)
-                        cssstyle.transform="translateX("+this.offset+"rem)"
-                    }
-                   
-                break;
-                case 'Up':
-                break;
-                case 'Down':
-                break;
-                }
-          console.log(evt)
-        },
-        initCardStore(id){
-            console.log(id)
-        },
-        getInput:function(card,event){
-            console.log(card.id)
-            console.log(event)
-        },
-        init:function(){
-            let setId = this.$route.params.id;
-            let context = new dbcontext("DB_Vue_FlashCard",2);
-            context.open('DB_Vue_FlashCard').set("Cards").getQuery({setId:setId}).then((data)=>{
-                // console.log(data)
-                this.$store.commit("initCards",data)
-                this.cards =this.$store.getters.AllCards
-                this.loading = false;
-            }).catch(function(data){
-                this.error = true;
-            })
-            // this.currentCardId = id;
-            // this.cards = this.$store.getters.AllCards
-            // console.log(this.$route.params)
-        },
-        slider:function(dir){
-            if(typeof dir!='undefined'){
-                if(dir===-1){
-                    
-                    if(this.currentCardId>=this.cards.length-1){
-                        this.currentCtiardId = this.cards.length;
-                    }else{
-                       
-                        this.currentCardId = this.cards[this.currentCardId+1].id
-                        this.isSlider=true; 
-                        let elem = document.getElementsByClassName("anim__slider")
-                         
-                        let cssstyle = elem.item(0).style
-                        this.offset=this.offset+(-21)
-                        cssstyle.transform="translateX("+this.offset+"rem)"
-                        
-                    }
-                }
-                else{
-                    if(this.currentCardId<=0){
-                        this.currentCardId=0*1;
-                    }else{
-                        this.currentCardId = this.cards[this.currentCardId-1].id
-                        this.isSlider=false;
-                        let elem = document.getElementsByClassName("anim__slider")
-                        let cssstyle = elem.item(0).style
-                        this.offset= this.offset+(21)
-                        cssstyle.transform="translateX("+this.offset+"rem)"
-                    }
-                }
-            }
+    init: function() {
+      let setId = this.$route.params.id;
+      let context = new dbcontext("DB_Vue_FlashCard", 2);
+      context
+        .open("DB_Vue_FlashCard")
+        .set("Cards")
+        .getQuery({ setId: setId })
+        .then(data => {
+          // console.log(data)
+          this.$store.commit("initCards", data);
+          this.cards = this.$store.getters.AllCards;
+          this.loading = false;
+        })
+        .catch(function(data) {
+          this.error = true;
+        });
+      // this.currentCardId = id;
+      // this.cards = this.$store.getters.AllCards
+      // console.log(this.$route.params)
+    },
+    slider: function(dir) {
+      if (typeof dir != "undefined") {
+        if (dir === -1) {
+          this.isSlider = true;
+          let elem = document.getElementsByClassName("anim__slider");
+          let cssstyle = elem.item(0).style;
+          this.offset = this.offset + -21;
+          cssstyle.transform = "translateX(" + this.offset + "rem)";
+        } else {
+          this.isSlider = false;
+          let elem = document.getElementsByClassName("anim__slider");
+          let cssstyle = elem.item(0).style;
+          this.offset = this.offset + 21;
+          cssstyle.transform = "translateX(" + this.offset + "rem)";
         }
+      }
     }
-}
+  }
+};
 </script>
 <style scoped>
-.container--template{
-    display:block;
-    height:24rem;
-    
+.container--template {
+  display: block;
+  height: 24rem;
 }
-.layout__container{
-    width: 100%; /* card width */
-    height:24rem;
-    display: flex;
+.layout__container {
+  width: 100%; /* card width */
+  height: 24rem;
+  display: flex;
 }
-.layout__viewport{
-    width:100%;
-    height:2rem;
-    overflow: hidden;
+.layout__viewport {
+  width: 100%;
+  height: 2rem;
+  overflow: hidden;
 }
-.layout__item{
-    margin-right: 2rem;
-    /* transform: translate(-50%,0); */
-    /* position: absolute; */
+.layout__item {
+  margin-right: 2rem;
+  /* transform: translate(-50%,0); */
+  /* position: absolute; */
 }
-.layout__item:first-child{
-    margin-left: 8rem;
+.layout__item:first-child {
+  margin-left: 8rem;
 }
-.anim__container{
-    height:100%;
-    margin-bottom: 4rem;
+.anim__container {
+  height: 100%;
+  margin-bottom: 4rem;
 }
-.anim__slider{
-    transition: all .5s;
-    position:absolute;
+.anim__slider {
+  transition: all 0.5s;
+  position: absolute;
 }
-.anim__slider--active{
-    transform: translateX(0)
+.anim__slider--active {
+  transform: translateX(0);
 }
-.anim__slider--reverse{
-    transform: translateX(0)
+.anim__slider--reverse {
+  transform: translateX(0);
 }
 </style>
 
