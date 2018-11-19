cda7724efb3951d4ddfa5eff1da3ab02afccfd87
adensW <1012790248@qq.com>
2018-11-19 16:26:08 +0800

    [feat] start ToDo List function

---
+++ src/components/todo/ToDo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ToDo",
+    data(){
+        return{
+            list:[{
+                id:"1",
+                value:"test",
+                checked:false,
+                lft:1,
+                right:2,
+                treeId:0
+            }]
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todo/ToDoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoItem",
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style>
+</style>
